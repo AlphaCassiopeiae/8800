2715a08d3c83c93c7e29fff03ef0bbff71a359eb
Caleb Shinkle <cshinkle@purdue.edu>
2025-02-24 11:35:41 -0500

    need HAL stuff added to recall

--- rs/recall/src/main.rs
+++ rs/recall/src/main.rs
@@ -1,7 +1,7 @@
 mod recall;
 use recall::RAM;
 
-fn main() {
+fn main() -> ! {
     let mut memory: RAM = RAM::new();
 
     // testing basic read/write
@@ -16,4 +16,17 @@ fn main() {
     let unset_addr: usize = 15;
     readval =  memory.read(unset_addr);
     println!("The value {} is at address {}", readval, unset_addr);
+
+    // this is where we will be checking if memory control signals are asserted
+    // respond accordingly (read or write)
+    // A0-A15: address pins
+    // D0-D7: data pins
+    // nPWR: low-active, bus signal confirming D0-D7 is valid
+    // MWRITE: identifies read operation
+    // SMEMR: identifes read operation
+    // PDBIN: control signal, tells memory to put read value on D0-D7
+    loop {
+        // comment this out when want to test
+        panic!("not done with micro stuff yet");
+    }
 }
